0630b33663de3d919a17de661c9af8ff19e469d9
wuhaohao <1611499758@qq.com>
2023-06-12 11:54:36 +0800

    feat: 添加老师

--- server/index.py
+++ server/index.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 import pymysql
 from student import get_all_students, get_student, add_student, update_student, delete_student
+from teacher import *
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'  # 数据库主机名
@@ -49,6 +50,38 @@ def update_student_route(student_id):
 def delete_student_route(student_id):
     result = delete_student(conn, student_id)
     return jsonify(result)
+
+# 路由 - 获取所有老师
+@app.route('/teachers', methods=['GET'])
+def get_all_teachers_route():
+    teachers = get_all_teachers(conn)
+    return jsonify(teachers)
+
+# 路由 - 添加老师
+@app.route('/teachers', methods=['POST'])
+def add_teacher_route():
+    teacher_data = request.get_json()
+    add_teacher(conn, teacher_data)
+    return jsonify({'message': 'Teacher added successfully'})
+
+# 路由 - 删除老师
+@app.route('/teachers/<int:teacher_id>', methods=['DELETE'])
+def delete_teacher_route(teacher_id):
+    delete_teacher(conn, teacher_id)
+    return jsonify({'message': 'Teacher deleted successfully'})
+
+# 路由 - 更新老师
+@app.route('/teachers/<int:teacher_id>', methods=['PUT'])
+def update_teacher_route(teacher_id):
+    teacher_data = request.get_json()
+    update_teacher(conn, teacher_id, teacher_data)
+    return jsonify({'message': 'Teacher updated successfully'})
+
+# 路由 - 查询单个老师
+@app.route('/teachers/<int:teacher_id>', methods=['GET'])
+def get_teacher_route(teacher_id):
+    teacher = get_teacher(conn, teacher_id)
+    return jsonify(teacher)
 
 if __name__ == '__main__':
     app.run()
---
+++ server/teacher.py
@@ -0,0 +1,52 @@
+import pymysql
+# 获取所有老师
+def get_all_teachers(conn):
+    try:
+        with conn.cursor() as cursor:
+            sql = "SELECT * FROM teachers"
+            cursor.execute(sql)
+            teachers = cursor.fetchall()
+            return teachers
+    finally:
+        conn.close()
+
+# 添加老师
+def add_teacher(conn, teacher_data):
+    try:
+        with conn.cursor() as cursor:
+            sql = "INSERT INTO teachers (name, email) VALUES (%s, %s)"
+            cursor.execute(sql, (teacher_data['name'], teacher_data['email']))
+            conn.commit()
+    finally:
+        conn.close()
+
+# 删除老师
+def delete_teacher(conn, teacher_id):
+    try:
+        with conn.cursor() as cursor:
+            sql = "DELETE FROM teachers WHERE id = %s"
+            cursor.execute(sql, (teacher_id,))
+            conn.commit()
+    finally:
+        conn.close()
+
+# 更新老师
+def update_teacher(conn, teacher_id, teacher_data):
+    try:
+        with conn.cursor() as cursor:
+            sql = "UPDATE teachers SET name = %s, email = %s WHERE id = %s"
+            cursor.execute(sql, (teacher_data['name'], teacher_data['email'], teacher_id))
+            conn.commit()
+    finally:
+        conn.close()
+
+# 查询单个老师
+def get_teacher(conn, teacher_id):
+    try:
+        with conn.cursor() as cursor:
+            sql = "SELECT * FROM teachers WHERE id = %s"
+            cursor.execute(sql, (teacher_id,))
+            teacher = cursor.fetchone()
+            return teacher
+    finally:
+        conn.close()
